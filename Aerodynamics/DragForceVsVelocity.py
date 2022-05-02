f93595f4ee8d3304b45792735f257c529f4e479a
William <69966592+WilliamTheDev@users.noreply.github.com>
2022-05-02 22:23:59 +1000

    Add files via upload

---
+++ Aerodynamics/DragForceVsVelocity.py
@@ -0,0 +1,43 @@
+import time
+
+startTime = time.time()
+Output = open('\dragCo1.txt', 'W') # didn't work? Insert a txt file Location inside the brackets
+
+velocity = 0 # Meter per second 
+density = 1.225 # kg/m^3
+area = 0 # Meters^2 
+
+DF = []
+MinCodrag = 0 # Min Coefficent of drag
+MaxCodrag = 0 # Max Coefficent of drag
+steps = 0.005 # Steps between min & max
+DataResolution = 100 # Loop times
+
+I = MinCodrag 
+r = 0 
+print("start")
+while I < (MaxCodrag+steps):
+    # Prints the new Coefficient Number 
+    RI = round(I, 5)
+    print("CoDrag: ", RI)
+    Output.writelines(" (CoDrag: " + str(RI)+") ")
+
+    i = 0 
+    velocity1 = velocity
+    while i < DataResolution: 
+        # Drag Force 
+        # = Coefficent of Drag * (density * velocity/2) * Surface area 
+        dragForce = I * (density*(velocity1*velocity1)/2)*area
+        DFOutput = round(dragForce, 5)
+        Output.writelines(" " + str(DFOutput))
+        i = i + 1
+        # Increases the velocity by +1 mps per loop
+        velocity1 = velocity + (i-velocity+1)
+        r = r + 1
+    I=I + steps
+
+
+# Shows the number of Calculations Done
+executionTime = (time.time() - startTime)
+print("Calculations performed =", round((I-(MinCodrag+steps))*100*DataResolution))
+print("Execution Time = ", str(executionTime), "seconds")
---
+++ Aerodynamics/Simple-Lift-&-Drag.py
@@ -0,0 +1,30 @@
+# This program calculates the lift and drag of an airplane.
+
+# Equations:
+# - Lift = Coefficient of lift * 0.50 * Density * Velocity² * WingSurfaceArea 
+# - Drag = 0.50 * Density * TotalSurfaceArea * Coefficient of lift * Velocity²
+
+# Wing Parameters
+WingSurfaceArea = 0 # Meters^2 (Wing Surface Area Only)
+WingSpan = 0 #Meters
+CoLift = 0
+
+# Airplane Parameters 
+TotalSurfaceArea = 0 # Meters^2 (Surface Area of the fuselage, Tail and Wing)
+CoDrag = 0
+
+# Simulation Parameters 
+Velocity = 0 # Meters per second (Speed of the plane)
+Density = 0 # kg/m^3 (Atmospheric pressure)
+
+def Lift(): # Lift Equation
+    Lift = CoLift * 0.50 * Density * pow(Velocity, 2) * WingSurfaceArea
+    return Lift
+
+def Drag(): # Drag Equation
+    Drag = 0.50 * Density * TotalSurfaceArea * CoDrag * pow(Velocity, 2)
+    return Drag 
+
+print("Outputs: ")
+print("Lift: " + str(Lift()) + " KG ")
+print("Drag: " + str(Drag()) + " KG ")
